030540f726d593f3beeab791bd5717725f947232
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-10-15 19:17:55 +0200

    init

---
+++ front/src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+function IndexPage() {
+  return (
+    <Layout>
+      <SEO
+        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
+        title="Home"
+      />
+
+      <section className="text-center">
+        <p>hello, welcome to my resizer</p>
+      </section>
+    </Layout>
+  );
+}
+
+export default IndexPage;
