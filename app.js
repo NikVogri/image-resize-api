4fcde46ca6a5ddae8b66ebe1cbe06a21aeda8018
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-10-18 21:55:46 +0200

    feat - resizing images implemented

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const rateLimit = require("express-rate-limit");
 const conversionRouter = require("./routes/conversionRouter");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // one minute,
-  max: 15, // 15 per minute
+  windowMs: 15 * 60 * 1000, // 15 minute,
+  max: 225, // 15 per minute
+  handler: (req, res) => {
+    res.status(429).json({
+      success: false,
+      msg: "Too many requests, please wait a while before trying again!",
+    });
+  },
 });
 
 app.use(limiter);
